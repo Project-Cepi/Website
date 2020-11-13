1f1d595f2e0866fef36f80a0552c945060aeb61e
LeoDog896 <LeoDog896@hotmail.com>
2020-11-13 14:30:24 -0500

    Added colors

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,15 @@
 <svelte:head>
-	<title>About</title>
+	<title>About Cepi</title>
 </svelte:head>
 
-<h1>About this site</h1>
+<h1>About Cepi</h1>
 
-<p>This is the 'about' page. There's not much here.</p>
+<p>
+	The MMORPG takes place in a vast fantasy world filled with magic and never before seen wonders.
+	Where you can venture with friends and face challenges, discover unique regions, and become a part of the immersive world.
+
+	Cepi's Development is different than many other projects similar to ours because we do not use bukkit or any forks,
+	and we have plans to become open source.
+	Our Development Team contains many talented individuals who have learned a lot and continue to grow while working on this project.
+	We continue to strive to improve and are focused on delivering the best results possible.
+</p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -37,7 +37,7 @@
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Cepi</title>
 </svelte:head>
 
 <h1>Great success!</h1>
@@ -47,4 +47,4 @@
 	<figcaption>Have fun with Sapper!</figcaption>
 </figure>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<p><strong>Welcome to Cepi!</strong></p>
