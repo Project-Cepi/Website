7d163f066b2da6d8123a5ea4001318f27d5d3850
LeoDog896 <LeoDog896@hotmail.com>
2020-11-13 21:30:51 -0500

    Add social media links

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -46,17 +46,25 @@
 		padding: 1em 0.5em;
 		display: block;
 	}
+
+	li.social {
+		float: right;
+		text-align: right;
+	}
 </style>
 
 <nav>
 	<ul>
 		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
 		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
-		<li><a href="https://github.cepi.world">Github</a></li>
-		<li><a href="https://discord.cepi.world">Discord</a></li>
 
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
 		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>
+
+		<li class="social"><a href="https://github.cepi.world">github</a></li>
+		<li class="social"><a href="https://discord.cepi.world">discord</a></li>
+		<li class="social"><a href="https://twitter.cepi.world">twitter</a></li>
+
 	</ul>
 </nav>
