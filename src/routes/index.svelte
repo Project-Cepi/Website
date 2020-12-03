60b8014b11692610119f7f07d8cf438fd5c2821c
LeoDog896 <LeoDog896@hotmail.com>
2020-12-03 08:48:30 -0500

    Nice background gradient

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,6 +13,14 @@
 		text-transform: uppercase;
 		font-weight: 700;
 		margin: 0 0 0.5em 0;
+		text-align: center;
+		background: linear-gradient(90deg, rgba(41,214,78,1) 0%, rgba(0,185,255,1) 50%, rgba(41,214,78,1) 100%);
+		background-clip: text;
+		background-size: 200% auto;
+		background-repeat: repeat;
+		text-align: center;
+		-webkit-background-clip: text;
+		-webkit-text-fill-color: transparent;
 	}
 	/*
 	figure {
@@ -40,5 +48,5 @@
 	<title>Cepi</title>
 </svelte:head>
 
-<h1>Cepi</h1>
-<p>A MMORPG with putting the MMO in RPG.</p>
+<h1>Putting the MMO in RPG</h1>
+<p>We're a team of builders, writers, game desginers, and developers, working together to make an RPG that is truly playable by you and your friends. </p>
