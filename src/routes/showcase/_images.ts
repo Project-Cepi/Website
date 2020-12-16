c4721daba3a2b99a11458506e4d180b67bc0d8d5
LeoDog896 <LeoDog896@hotmail.com>
2020-12-16 10:18:02 -0500

    Example image in showcase

--- src/routes/showcase/_images.ts
+++ src/routes/showcase/_images.ts
@@ -1,1 +1,15 @@
+interface ImageShowcase {
+	url: string;
+	description?: string;
+	creators: string[];
+}
+
+const images: ImageShowcase[] = [
+	{
+		url: "https://cdn.discordapp.com/attachments/707787042750201906/746181584025026610/2020-08-20_20.31.46.png",
+		creators: ["savagechick3n", "LeoDog896"],
+		description: "Retextured unused slabs turned dirty."
+	}
+];
+
 export default {}
