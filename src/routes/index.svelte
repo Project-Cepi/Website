7778bb8210b7b827778bf7cf6bb93d53b37cb90d
LeoDog896 <LeoDog896@hotmail.com>
2021-12-16 19:44:06 -0500

    Space out classes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,5 @@
-<div id="main" class="w-screen h-screen flex flex-col">
-	<div id="container" class="grid place-items-center h-screen">
-		<h1 class="font-extrabold text-9xl text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-green-300">Cepi</h1>
-		<iframe title="Cepi Trailer" class="h-full aspect-video rounded-lg shadow-lg" src="https://www.youtube.com/embed/2VJqyIaiUD8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-		<h2 class="font-extrabold text-6xl text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-green-300">The creative MMORPG.</h2>
-	</div>
+<div id="main" class="w-screen h-screen justify-evenly content-around flex flex-col items-center text-center">
+	<h1 class="font-extrabold text-9xl text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-green-300">cepi.world</h1>
+	<iframe title="Cepi Trailer" class="h-1/2 aspect-video rounded-lg shadow-lg" src="https://www.youtube.com/embed/2VJqyIaiUD8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	<button>...</button>
 </div>
