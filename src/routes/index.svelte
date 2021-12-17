9c2dc7d5a3e746c376f7e02d81bdfa8676ecf5f9
LeoDog896 <LeoDog896@hotmail.com>
2021-12-17 16:54:14 -0500

    Logo fixes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,6 +10,11 @@
 		component: any
 	}
 
+	interface Link {
+		name: string,
+		url: string
+	}
+
 	const socialLinks: Social[] = [
 		{
 			url: "https://discord.cepi.world",
@@ -32,14 +37,33 @@
 			component: GithubLogo
 		}
 	]
+
+	const normalLinks: Link[] = [
+		{
+			name: "yard",
+			url: "https://project-cepi.github.io"
+		},
+		{
+			name: "dev",
+			url: "https://cepi.world/dev"
+		}
+	]
 </script>
 
 <div id="main" class="animate-gradient-x bg-gradient-to-r from-cyan-300 to-green-300 w-screen h-screen justify-evenly content-around flex flex-col items-center text-center">
 	<h1 class="font-extrabold text-body drop-shadow-lg text-9xl text-white">cepi<span class="text-black">.</span>world</h1>
-	<iframe title="Cepi Trailer" class="h-1/2 aspect-video rounded-lg shadow-lg" src="https://www.youtube.com/embed/2VJqyIaiUD8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	<iframe title="Cepi Trailer" class="h-1/2 aspect-video rounded-lg shadow-lg border-8 border-white" src="https://www.youtube.com/embed/2VJqyIaiUD8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 	<div id="socials" class="flex h-12">
 		{#each socialLinks as {url, component}}
 			<a class="m-1" href={url}><svelte:component this={component}/></a>
+		{/each}
+	</div>
+	<div id="links" class="flex h-12 drop-shadow-xl">
+		{#each normalLinks as {name, url}, i}
+			<a class="font-bold text-white text-3xl" href={url}>{name}</a>
+			{#if normalLinks.length - 1 !== i}
+				<span class="text-white text-3xl mx-5">|</span>
+			{/if}
 		{/each}
 	</div>
 </div>
